1d54c9059784b6ab68ab85e564bf267b9add8638
Biswajit Basak <juelbasak@gmail.com>
2021-08-17 22:30:26 +0530

    Changes done in HTML and CSS

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def homepage():
     return render_template('index.html')
-def predict():
-    pass
 
+@app.route("/predict", methods=['POST'])
+def predict():
+    fixed_acidity = request.form['fixed_acidity']
+    volatile_acidity = request.form['volatile_acidity']
+    citric_acid = request.form['citric_acid']
+    residual_sugar = request.form['residual_sugar']
+    chlorides = request.form['chlorides']
+    free_sulfur_dioxide = request.form['free_sulfur_dioxide']
+    total_sulfur_dioxide = request.form['total_sulfur_dioxide']
+    density = request.form['density']
+    pH = request.form['pH']
+    sulphates = request.form['sulphates']
+    alcohol = request.form['alcohol']
+
+    model = joblib.load('./model/new_tree.pkl')
+
+    prediction = model.predict([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]])
+    print(prediction)
+    return render_template('index.html', prediction=prediction)
+
+     
 if __name__ == '__main__':
     app.run(debug=True)
