0018ed831ba5d76ccc447b8856c5af04248f1797
Biswajit Basak <juelbasak@gmail.com>
2021-08-18 18:26:14 +0530

    main file changed for debugging

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ def homepage():
     print('Working on homepage method')
     return render_template('test.html')
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    fixed_acidity = request.form['fixed_acidity']
-    volatile_acidity = request.form['volatile_acidity']
-    citric_acid = request.form['citric_acid']
-    residual_sugar = request.form['residual_sugar']
-    chlorides = request.form['chlorides']
-    free_sulfur_dioxide = request.form['free_sulfur_dioxide']
-    total_sulfur_dioxide = request.form['total_sulfur_dioxide']
-    density = request.form['density']
-    pH = request.form['pH']
-    sulphates = request.form['sulphates']
-    alcohol = request.form['alcohol']
+# @app.route("/predict", methods=['POST'])
+# def predict():
+#     fixed_acidity = request.form['fixed_acidity']
+#     volatile_acidity = request.form['volatile_acidity']
+#     citric_acid = request.form['citric_acid']
+#     residual_sugar = request.form['residual_sugar']
+#     chlorides = request.form['chlorides']
+#     free_sulfur_dioxide = request.form['free_sulfur_dioxide']
+#     total_sulfur_dioxide = request.form['total_sulfur_dioxide']
+#     density = request.form['density']
+#     pH = request.form['pH']
+#     sulphates = request.form['sulphates']
+#     alcohol = request.form['alcohol']
 
-    model = joblib.load('./model/new_tree.pkl')
+#     model = joblib.load('./model/new_tree.pkl')
 
-    prediction = model.predict([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]])
-    if prediction[0] < 5:
-        return render_template('output1.html', prediction=prediction[0])
-    return render_template('output2.html', prediction=prediction[0])
+#     prediction = model.predict([[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]])
+#     if prediction[0] < 5:
+#         return render_template('output1.html', prediction=prediction[0])
+#     return render_template('output2.html', prediction=prediction[0])
 
 
 if __name__ == '__main__':
